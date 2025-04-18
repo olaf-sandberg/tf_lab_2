8123450703ae2304a7c1a6531fa88983f740f2fb
olaf-sandberg <sandberg.olaf@gmail.com>
2025-04-18 12:41:18 +0200

    minor bug fix in main.tf / compute

--- vars.tf
+++ vars.tf
@@ -62,13 +62,3 @@ variable "private_sg_id" {
   type        = string
 }
 
-variable "project_name" {
-  description = "Project name for tagging"
-  type        = string
-}
-
-variable "environment" {
-  description = "Environment name (e.g., pol, ger)"
-  type        = string
-}
-
