76e699260648207ebb20e7006f1e0b26a46ba3fb
olaf-sandberg <sandberg.olaf@gmail.com>
2025-04-25 07:21:55 +0200

    variables passing between network --> main --> compute amended to be more dynamic

--- compute/main.tf
+++ compute/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "public" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  subnet_id                   = var.public_subnet_id
+  subnet_id                   = var.public_subnet_ids[0]
   vpc_security_group_ids      = [var.public_sg_id]
   associate_public_ip_address = true
   key_name                    = var.key_name
@@ -14,11 +14,27 @@ resource "aws_eip" "bastion_eip" {
   domain   = "vpc"
 }
 
+# Public EC2 Instance (Wwebeb)
+resource "aws_instance" "web" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.public_subnet_ids[1]
+  vpc_security_group_ids      = [var.public_sg_id]
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+}
+
+# Elastic IP for Public Instance
+resource "aws_eip" "web_eip" {
+  instance = aws_instance.web.id
+  domain   = "vpc"
+}
+
 # Private EC2 Instance 1
 resource "aws_instance" "private1" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id
+  subnet_id              = var.private_subnet_ids[0]
   vpc_security_group_ids = [var.private_sg_id]
   key_name               = var.key_name
 }
@@ -27,7 +43,7 @@ resource "aws_instance" "private1" {
 resource "aws_instance" "private2" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id
+  subnet_id              = var.private_subnet_ids[0]
   vpc_security_group_ids = [var.private_sg_id]
   key_name               = var.key_name
 }
@@ -36,16 +52,16 @@ resource "aws_instance" "private2" {
 resource "aws_instance" "private3" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id2
+  subnet_id              = var.private_subnet_id[1]
   vpc_security_group_ids = [var.private_sg_id]
   key_name               = var.key_name
 }
 
-# Private EC2 Instance 24
+# Private EC2 Instance 4
 resource "aws_instance" "private4" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  subnet_id              = var.private_subnet_id2
+  subnet_id              = var.private_subnet_id[1]
   vpc_security_group_ids = [var.private_sg_id]
   key_name               = var.key_name
 }
